2604d582bcf8083621f521197028e3ca1e9e4624
dbwogus94 <1994dbwogus@gmail.com>
2020-06-28 05:20:50 +0900

    FIX : (FIX ) 직접 구현한 비동기 전송 함수문제 해결함
    - 문제점에 대해 구체적으로 commentController에 명시함
    - 문제해결후 get방식 테스트용으로 컨트롤러 메서드 추가(loadCommentList_GET())

--- SEMI_spring/src/main/java/com/semi/update/controller/CommentController.java
+++ SEMI_spring/src/main/java/com/semi/update/controller/CommentController.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,21 +30,58 @@ public class CommentController {
 	
 	// ALL 비동기
 	
+	/* **비동기 사용 주의점** 
+	 * 
+	 * 	# view 로직
+	 *  	1. view ajax(비동기) post 방식으로 전송 방식 선택
+	 * 		2. 전송시 타입 "Content-type = application/json"으로 추가
+	 *  	3. 전송시 body(request payload)에 JSON.stringify()를 이용하여 json형태의 스트링으로 변환하여 전송
+	 *  	>>> json을 스트링으로 변환하지 않고 전송하면 request body(request payload)를 사용하는것이 아니라
+	 *  		쿼리스트링 방식(request parameter)을 사용하는 것이다. 
+	 *  
+	 *  # 서버 로직 
+	 *		1. 스프링에서 @ResponseBody을 사용하여 비동기 통신을 수행할 수 있다.
+	 *		2. view에서 post방식으로 jsonObject를 json스트링으로 변환하여 전송(request payload를 사용한 것)
+	 *			이럴때는 @RequestParam 또는  @ModelAttribute을 사용할 수 없다. 
+	 *		   	즉, @RequestBody만 사용이 가능
+	 *	
+	 *		3. 문제는 @RequestBody는 jackson을 사용하여 매핑을 받는다. 
+	 *			더 정확히 말하면 jackson은 직열화를 사용하여 json형태의 스트링을 메핑한다.
+	 *			즉, 직열화가 되지 못하는 단일 값에는 @RequestBody를 사용하지 못한다. 
+	 *			(@RequestBody String boardNo >> 이런 경우 안됨.)
+	 *			단일의 값을 받고 싶다면 dto Class를 만들어서 받던가 또는 
+	 *			map 컬렉션 클래스를 사용하여 받으면 된다.
+	 *
+	 *		결론 : json형태의 스트링을 서버에서 받을때 단일 값 OR dto가 없는 다중값을 받고 싶다면 
+	 *			dto Class를 새로 만들던가, HashMap를 사용해서 받는다.
+	 *			ex) @RequsetBody HashMap<String, String> input   
+	 *
+	 *
+	 *  # 서버에서 비동기전송된 데이터 받는법
+	 *		1. 일반적으로 get방식으로 받을때는 @RequestParam을 사용해서 받으면 된.
+	 *		2. form태그에서 post로 전송이 될경우 @ModelAttribute를 통하여 받으면 된다. 
+	 *		3. {}(jsonObject)가 아닌 단일 값을 post방식으로 보낸다면 @ModelAttribute를 사용하면 된다.    	
+	 *		4. {}(jsonObject)로 전송된 값에 대응하는 dto가 있다면 @RequestBody를 사용하여 받으면 된다.
+	 * 		>>> 이는  jackson에 의해 jsonObject에 대응하는 dto를 찾아서 메핑해주기 때문에 가능하다 
+	 * 		>>> 더 정확하게 말하면 dto Class를 직렬화 하여 json에 대응하는 dto를 찾아서 메핑이 되는것이다. 
+	 * 
+	 */
+	
 	// 댓글 리스트 보기  >>> 뷰변경
-	@RequestMapping(value = "loadCommentList.do", method = {RequestMethod.GET, RequestMethod.POST}) // produces = "application/json; charset=utf8", headers = "content-type=application/json"
+	@RequestMapping(value = "loadCommentList.do", method = RequestMethod.POST) // produces = "application/json; charset=utf8", headers = "content-type=application/json"
 	@ResponseBody
-	public Map<String, List<CommentDto>> loadCommentList(@RequestParam("boardNo") int boardNo, @RequestParam("currentPage") int currentPage, CommentDto commentDto){
+	public Map<String, List<CommentDto>> loadCommentList(@RequestBody HashMap<String, Integer> input, CommentDto commentDto){
 		//@ModelAttribute("jsonObject") HashMap<String, Integer> jsonObject
-		logger.info("[ajax] load Comment List input : " + boardNo);
+		logger.info("[ajax] load Comment List input : " + input);
 		Map<String, List<CommentDto>> output = new HashMap<String, List<CommentDto>>();
 		
 		// 답글 dto에 부모글 시퀀스 추가
-		commentDto.setBoardNo(boardNo);
+		commentDto.setBoardNo(input.get("boardNo"));
 		// 부모글의 답글 총개수 
 		int totalComment = commentBiz.count_commentList(commentDto); 
 				
 		// 페이징 객체 생성(화면에 출력할 답글수(10, 출력될 페이지(여기서 필요없음), 총개수, 현재 페이지(1)
-		OraclePagination pagination = new OraclePagination(10, 10, totalComment, currentPage);
+		OraclePagination pagination = new OraclePagination(10, 10, totalComment, input.get("currentPage"));
 				
 		// 페이징 시작번호
 		commentDto.setStartBoardNo(pagination.getStartBoardNo());
@@ -59,6 +96,34 @@ public class CommentController {
 		return output;
 	}
 	
+	// 댓글 리스트 보기  >>> 뷰변경
+	@RequestMapping(value = "loadCommentList_GET.do", method = RequestMethod.GET) // produces = "application/json; charset=utf8", headers = "content-type=application/json"
+	@ResponseBody
+	public Map<String, List<CommentDto>> loadCommentList_GET(int boardNo, int currentPage, CommentDto commentDto){
+		//@ModelAttribute("jsonObject") HashMap<String, Integer> jsonObject
+		logger.info("[ajax] load Comment List input : " + boardNo);
+		Map<String, List<CommentDto>> output = new HashMap<String, List<CommentDto>>();
+		
+		// 답글 dto에 부모글 시퀀스 추가
+		commentDto.setBoardNo(boardNo);
+		// 부모글의 답글 총개수 
+		int totalComment = commentBiz.count_commentList(commentDto); 
+				
+		// 페이징 객체 생성(화면에 출력할 답글수(10, 출력될 페이지(여기서 필요없음), 총개수, 현재 페이지(1)
+		OraclePagination pagination = new OraclePagination(10, 10, totalComment, currentPage);
+					
+		// 페이징 시작번호
+		commentDto.setStartBoardNo(pagination.getStartBoardNo());
+		// 페이징 끝번호
+		commentDto.setEndBoardNo(pagination.getEndBoardNo());
+					
+		// 현재 게시물의 댓글 가져오기
+		List<CommentDto> commentList = commentBiz.commentList(commentDto);
+					
+		output.put("commentList", commentList);
+		logger.info("[ajax] load Comment List output : " + output);
+		return output;
+	}
 	
 	// 대댓글 리스트 보기
 	
--- SEMI_spring/src/main/webapp/resources/js/board/BOARD_boardComment.js
+++ SEMI_spring/src/main/webapp/resources/js/board/BOARD_boardComment.js
@@ -22,30 +22,31 @@ window.onscroll = function(e){
 		// 비동기로 서버에 요청시 사용할 데이터
 		let jsonObject = {boardNo:Number(boardNo), currentPage:currentPage}
 		
+		// post 	
+		//const xhr_param = new Xhr_param("comment/loadCommentList.do", "POST", "json", "application/json; charset=utf-8", jsonObject)
 		
-		// async_xhr에 파라미터에 사용되는 클래스 	
-		//const xhr_param = new Xhr_param("comment/loadCommentList.do", "POST", "json", "application/json", jsonObject)
-		//xhr_param.setContentType("application/json")
-		/* 문제 발생 : 
-		 * post로 전송하여 사용히고 싶었지만 서버에서 
-		 * xhr_param에 contentType = "application/json" 
-		 * 이 적용이 안되고 있음
-		 * 그래서 get방식으로 대처를했다. 
-		 */
-		
-		
-		const xhr_param = new Xhr_param("comment/loadCommentList.do", "GET", "json", undefined, jsonObject)
-		console.log(xhr_param)
+		// get
+		//const xhr_param = new Xhr_param("comment/loadCommentList_GET.do", "GET", "json", undefined, jsonObject)
+		//console.log(xhr_param)
 		
 		// 비동기
-		async_xhr(xhr_param, loadCommentList)
+		//async_xhr(xhr_param, loadCommentList)
+		
+		//post
+		const xhr = new Xhr("comment/loadCommentList.do", "POST", "json", "application/json; charset=utf-8", jsonObject)
+		xhr.async_POST(loadCommentList)
+		
+		//get
+		//const xhr = new Xhr("comment/loadCommentList_GET.do", "GET", "json", undefined, jsonObject)
+		//xhr.async_GET(loadCommentList)
 	}
 	 
 }
 
 
 
-// XMLHTTpRequest
+// XMLHTTpRequest		>> 리팩토링 하자 : get, post 둘다 이미지 받는거 가능하게 처리, post방식에 body로 보낼값 없을때 .send() 추가할 것
+// async_xhr와 Xhr_param 합치기
 function async_xhr(Xhr_param, callBack){
 	// #1
 	const xhr = new XMLHttpRequest();
@@ -55,8 +56,7 @@ function async_xhr(Xhr_param, callBack){
 		if(xhr.readyState === xhr.DONE){		// xhr.DONE는 서버에서 정상 응답되었을때
 			if (xhr.status === 200 || xhr.status === 201) {
 				// 응답시 받은 데이터 확인
-				console.log("성공 메세지" + xhr.response);
-				console.log("성공 데이터 확인" + xhr.response);
+				console.log("성공 데이터 확인 : " + JSON.stringify(xhr.response));
 				
 				// 데이터 콜백에 전달
 				if(callBack){
@@ -74,13 +74,14 @@ function async_xhr(Xhr_param, callBack){
 	// post방식
 	if(Xhr_param.getMethod() === "POST" || Xhr_param.getMethod() === "post"){
 		
-		if(Xhr_param.getContentType() === "application/json"){		// Content-type가 쿼리스트링이 아니라면
+		if(Xhr_param.getContentType() === "application/json; charset=utf-8"
+			|| Xhr_param.getContentType() === "application/json"){		// Content-type가 쿼리스트링이 아니라면
 			// #2
 			xhr.open(Xhr_param.getMethod(), Xhr_param.getUrl());					// 열기 : 요청할 url 메소드 설정 
 			
 			xhr.responseType = Xhr_param.getResponseType();							// 응답 받을 타입 설정
 			
-			xhr.setRequestHeader('Content-type', Xhr_param.setContentType());		// 전송 방식 설정 : Content-type = application/json
+			xhr.setRequestHeader('Content-type', Xhr_param.getContentType());		// 전송 방식 설정 : Content-type = application/json
 			
 			console.log(JSON.stringify(Xhr_param.getJsonObject()));
 			//#3 
@@ -92,7 +93,8 @@ function async_xhr(Xhr_param, callBack){
 	
 	// GET방식	
 	} else if(Xhr_param.getMethod() === "GET" || Xhr_param.getMethod() === "get") {
-		if(Xhr_param.getContentType() === "application/x-www-form-urlencoded"){
+		if(Xhr_param.getContentType() === "application/x-www-form-urlencoded; charset=utf-8"
+			|| Xhr_param.getContentType() === "application/x-www-form-urlencoded"){
 			
 			// 쿼리스트링 있을때
 			if(Xhr_param.getJsonObject() !== {}){
@@ -108,9 +110,8 @@ function async_xhr(Xhr_param, callBack){
 				
 				xhr.responseType = Xhr_param.getResponseType();							// 응답 받을 타입 설정
 				
-				// #3
+				
 				console.log("GET 방식 전송 : 쿼리스트링 있음 >> " + Xhr_param.getUrl() + "?" + query)
-				xhr.send();																// 전송									
 				
 			// 쿼리스트링이 없을때
 			} else {
@@ -118,11 +119,11 @@ function async_xhr(Xhr_param, callBack){
 				xhr.open(Xhr_param.getMethod(), Xhr_param.getUrl());
 				xhr.responseType = Xhr_param.getResponseType();
 				
-				// #3
-				console.log("GET 방식 전송 : 쿼리스트링 없음")
-				xhr.send();
 				
+				console.log("GET 방식 전송 : 쿼리스트링 없음")
 			}
+			// #3
+			xhr.send();		// get방식 전송		
 			
 		} else {
 			console.log("Content-Type이 맞지 않습니다.")
@@ -146,22 +147,23 @@ function async_xhr(Xhr_param, callBack){
 
 
 function loadCommentList(response){	
-
-	//let dto = new CommentDto(response)
 	console.log(response)
+
+	let commentList = new Array();
+
+	for(let i = 0; i<response.length; i++){
+		commentList[i] = new CommentDto(response[i])
+	}
+	console.log(commentList)
+	
 	
 	/* 여기까지 확인  */
 	
-	// response.commentList를 for문 돌려서 CommentDto에 담아준다.
-	
-	// CommentDto를 이용하여 뷰를 그린다.
+	// commentList를 이용하여 뷰를 그린다.
+	// 뷰그리는 함수()
 }
 
 
-
-
-
-/* 미완성 */
 class CommentDto{
 	constructor(response) {
 		this.commentNo = response.commentNo;
@@ -179,6 +181,97 @@ class CommentDto{
 		this.endBoardNo = response.endBoardNo;
 	}
 	// + get set
+	getCommentNo() {
+		return this.commentNo;
+	}
+	setCommentNo(commentNo){
+		this.commentNo = commentNo;
+	}
+	
+	getBoardNo(){
+		return this.boardNo;
+	}
+	setBoardNo(boardNo){
+		this.boardNo = boardNo;
+	}
+	
+	getCommentGroupNo(){
+		return this.commentGroupNo;
+	}
+	setCommentGroupNo(commentGroupNo){
+		this.commentGroupNo = commentGroupNo;
+	}
+	
+	getCommentGroupSeq(){
+		return this.commentGroupSeq;
+	}
+	setCommentGroupSeq(commentGroupSeq){
+		this.commentGroupSeq = commentGroupSeq
+	}
+	
+	getProfileImg(){
+		return this.profileImg;
+	}
+	setProfileImg(profileImg){
+		this.profileImg = profileImg;
+	}
+	
+	getId(){
+		return this.id;
+	}
+	setId(id){
+		this.id = id
+	}
+	
+	getCommentName(){
+		return this.commentName;
+	}
+	setCommentName(commentName){
+		this.commentName = commentName;
+	}
+
+	getCommentContent(){
+		return this.commentContent;
+	}
+	setCommentContent(commentContent){
+		this.commentContent = commentContent;
+	}
+	
+	getCommentGegdate(){
+		return this.commentGegdate;
+	}
+	setCommentGegdate(commentGegdate){
+		this.commentGegdate = commentGegdate;
+	}
+	
+	getCommentUpdateRegDate(){
+		return this.commentUpdateRegDate;
+	}
+	setCommentUpdateRegDate(commentUpdateRegDate){
+		this.commentUpdateRegDate = commentUpdateRegDate
+	}
+	
+	getReCommentCount(){
+		return this.reCommentCount;
+	}
+	setReCommentCount(reCommentCount){
+		this.reCommentCount = reCommentCount;
+	}
+	
+	getStartBoardNo(){
+		return this.startBoardNo;
+	}
+	setStartBoardNo(startBoardNo){
+		this.startBoardNo = startBoardNo;
+	}
+	
+	getEndBoardNo(){
+		return this.endBoardNo;
+	}
+	setEndBoardNo(endBoardNo){
+		this.endBoardNo = endBoardNo;
+	}
+	
 }
 
 
@@ -188,7 +281,7 @@ class Xhr_param {
     	this.url = url;
 		this.method = method || "GET";
 		this.responseType = responseType || "json";
-		this.contentType = contentType || "application/x-www-form-urlencoded";	// get 방식 디폴트 == 쿼리스트링
+		this.contentType = contentType || "application/x-www-form-urlencoded; charset=utf-8";	// get 방식 디폴트 == 쿼리스트링
 		this.jsonObject = jsonObject || {}
     }
 
@@ -228,7 +321,6 @@ class Xhr_param {
 		this.jsonObject = jsonObject;
 	}
 	
-	
 	static func01(){
 		return "자바스크립트 클래스의 static이 붙은 함수는 자바의 static함수와 동일하게 사용됩니다. " +
 				"즉 클래스를 new로 생성하지 않고 사용이 가능합니다. " +
@@ -262,3 +354,198 @@ function Xhr_param2(url, method, responseType){
 	}
 	
 };
+
+
+
+
+
+
+
+
+
+
+
+
+// 비동기 전송용 자바스크립트 클래스
+// 생성자 인자전달에 http 설정값 전달.
+// 메소드 : 기본적인 get방식, post방식 구현, url >> base64 코드로 받는 메서드도 구현 예정  
+class Xhr {
+	// 생성자
+    constructor(url, method, responseType, contentType, jsonObject) {
+    	// #1
+    	this.xhr = new XMLHttpRequest();			// 자바스크립트의 비동기를 지원하는 객체
+    	this.url = url;
+		this.method = method || "GET";
+		this.responseType = responseType || "json";
+		this.contentType = contentType || "application/x-www-form-urlencoded; charset=utf-8";	// get 방식 디폴트 == 쿼리스트링
+		this.jsonObject = jsonObject || {}
+		
+    }
+
+    getUrl() {
+	    return this.url;
+	}    
+	setUrl(url){
+		this.url = url;
+	}
+	
+	getMethod(){
+		return this.method;
+	}
+	setMethod(method){
+		this.method = method;
+	}
+	
+	getResponseType(){
+		return this.responseType;
+	}
+	setResponseType(responseType){
+		this.responseType = responseType
+	}
+	
+	getContentType(){
+		return this.contentType;
+	}
+	setContentType(contentType){
+		this.contentType = contentType
+	}
+	
+	getJsonObject(){
+		return this.jsonObject;
+	}
+	setJsonObject(jsonObject){
+		this.jsonObject = jsonObject;
+	}
+	
+	// 일반적인 비동기 GET. POST 요청후 응답 
+	async_DataLoad(callBack){
+		const xhr = this.xhr
+		
+		// #4 onreadystatechange : 준비상태(200, 404, 500...)가 바뀌었을떄 트리거 되는 이벤트
+		xhr.onreadystatechange = function() { 
+			if(xhr.readyState === xhr.DONE){		// xhr.DONE는 서버에서 정상 응답되었을때
+				if (xhr.status === 200 || xhr.status === 201) {
+					// 응답시 받은 데이터 확인
+					console.log("성공 데이터 확인 : " + JSON.stringify(xhr.response));
+					
+					// 데이터 콜백에 전달
+					if(callBack){
+						callBack(xhr.response)
+					}
+				
+				} else {
+					console.error("서버 에러 : " + xhr.response);
+				}
+			} else if(xhr.readyState === 4){		// 네트워크 문제 같은것으로 서버에 가지 못했을때 404 
+				console.error("네트워크 에러 : " + xhr.responseText);
+			}
+		}
+		/*
+	 	request후에 웹에서 response가 트리거되는 순서
+	 
+		     onreadystatechange
+		 readyState === 4, xhr.DONE
+		                              ⇓
+		 onload / onerror / onabort
+		                              ⇓
+		         onloadend 
+	  
+	 */
+	}
+	
+	// 비동기 GET방식
+	async_GET(callBack){
+		
+		callBack = callBack || false;
+		
+		// #4 응답 >>> 응답 데이터 받기
+		this.async_DataLoad(callBack);
+		
+		if(this.getMethod() === "GET" || this.getMethod() === "get") {
+			if(this.getContentType() === "application/x-www-form-urlencoded; charset=utf-8"
+				|| this.getContentType() === "application/x-www-form-urlencoded"){
+				
+				if(this.getJsonObject() === {}){
+					// #2. 열기 : 쿼리스트링 없음
+					this.xhr.open(this.getMethod(), this.getUrl());				
+					console.log("GET 방식 전송 : 쿼리스트링 없음")
+				} else {
+					let query = "";
+					let params = this.getJsonObject();
+					for (let key in params){
+						query += key + "=" + params[key] + "&";
+					}
+					// #2.  열기 : 쿼리스트링 있음 >>> 요청할 url + 쿼리스트링
+					this.xhr.open(this.getMethod(), this.getUrl() + "?" + query);		
+					console.log("GET 방식 전송 : 쿼리스트링 있음 >> " + this.getUrl() + "?" + query)
+				}
+				
+				// #2. 응답 받을 타입 설정
+				this.xhr.responseType = this.getResponseType();				
+				
+			} else {
+				console.log("Content-Type이 맞지 않습니다. >>> get방식이면 'application/x-www-form-urlencoded; charset=utf-8' 으로 입력하세요. ")
+			} 
+			console.log("GET 방식으로 요청하세요")
+		}
+		// #3. 전송 : get방식 전송	
+		this.xhr.send();
+	}
+	
+	// 비동기 POST방식
+	async_POST(callBack){
+		
+		callBack = callBack || false;
+		
+		// #4 응답 >>> 응답 데이터 받기
+		this.async_DataLoad(callBack);
+		
+		if(this.getMethod() === "POST" || this.getMethod() === "post"){
+			if(this.getContentType() === "application/json; charset=utf-8"
+				|| this.getContentType() === "application/json"){		// Content-type가 쿼리스트링이 아니라면
+				
+				// #2. 열기 : 메소드 설정, 요청할 url 설정 
+				this.xhr.open(this.getMethod(), this.getUrl());		
+				// #2. 응답 받을 타입 설정
+				this.xhr.responseType = this.getResponseType();		
+				// #2. 전송 방식 설정 : Content-type = application/json
+				this.xhr.setRequestHeader('Content-type', this.getContentType());		
+			
+			} else {
+				console.log("Content-Type가 맞지 않습니다. >>> post방식이면 'application/json; charset=utf-8'으로 입력하세요. ")
+			}
+		} else {
+			console.log("POST 방식으로 요청하세요")
+		}
+		
+		if(this.getJsonObject() === {}){
+			// #3. 전송  : body없음
+			this.xhr.send();										
+		} else {
+			console.log(JSON.stringify(this.getJsonObject()));
+			// #3. 전송 : body있음 >>> post 방식일때는 보낼 데이터를 send(body)에 인자로 전달한다 >> Request payload. 
+			this.xhr.send(JSON.stringify(this.getJsonObject()));
+		}
+		
+	}
+	
+	// 정적자원 uri요청 >> base64 받아서 file 객체로 변환  
+	async_toDataURL(callBack){
+		
+	}
+	
+	//이미지 변환 : base64 >> new File(데이터, 이름 , 타입)
+	base64toFile(){
+		
+	}
+		
+	
+	static func01(){
+		return "자바스크립트 클래스의 static이 붙은 함수는 자바의 static함수와 동일하게 사용됩니다. " +
+				"즉 클래스를 new로 생성하지 않고 사용이 가능합니다. " +
+				" ex) Xhr_param1.func01() ";
+		
+	}
+}
+
+
